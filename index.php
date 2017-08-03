3e3d0583cedbe4bdf651daeaae9533658cb2eaed
Roman <roman.lossin@icloud.com>
2017-08-03 11:14:14 +0200

    FIX: If modal is not sticky, it's now shown only once

--- index.php
+++ index.php
@@ -100,11 +100,33 @@ return [
 									$paramstring
 								});";
 						} else {
-							$script = " function adBlockDetected() {
+							$script = "
+										function setCookie(cname, cvalue, exdays) {
+                                            var d = new Date();
+											d.setTime(d.getTime() + (exdays*24*60*60*1000));
+											var expires = 'expires='+d.toUTCString();
+											document.cookie = cname + '=' + cvalue + '; ' + expires;
+										}
+
+										function getCookie(cname) {
+										    var name = cname + '=';
+										    var ca = document.cookie.split(';');
+										    for(var i=0; i<ca.length; i++) {
+										        var c = ca[i];
+										        while (c.charAt(0)==' ') c = c.substring(1);
+										        if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+											}
+										    return '';
+										}";
+							
+							$script .= "
+								function adBlockDetected() {
 									var s = '$html';
 									var parser = new DOMParser();
 									var doc = parser.parseFromString( s, 'text/html' );
-									var detectionmodal = UIkit.modal.alert(doc.body.innerHTML);
+									if(!getCookie('detectadblockershown')){
+									setCookie('detectadblockershown', 'true', 365);
+									var detectionmodal = UIkit.modal.alert(doc.body.innerHTML); }
 					            }
 								if(typeof blockAdBlock === 'undefined') {
 									adBlockDetected();
@@ -132,8 +154,7 @@ return [
 				'detectadblocker-settings',
 				'spqr/detectadblocker:app/bundle/detectadblocker-settings.js',
 				[ '~extensions', 'input-tree', 'editor' ]
-			);
-		}
+			);		}
 	]
 
 ];
